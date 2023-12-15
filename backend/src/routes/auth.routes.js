7aaf15d7bc3a4acbb0c54ccb521b09ff2ca2bebf
Santiago Argamonte <santiagoargamonte@gmail.com>
2023-12-15 15:34:57 -0300

    agregada validaciones en login y registro

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -6,14 +6,15 @@ import {
   profile,
 } from "../controllers/auth.controller.js";
 import { authRequired } from "../middlewares/validateToken.js";
+import {validateLogin, validateRegister,handleErrorValidations} from "../middlewares/validateAuth.js"
 
 const routes = Router();
 
 //registro
-routes.post("/register", register);
+routes.post("/register", validateRegister, handleErrorValidations, register);
 
 //login
-routes.post("/login", login);
+routes.post("/login", validateLogin, handleErrorValidations, login);
 
 routes.post("/logout", logout);
 
